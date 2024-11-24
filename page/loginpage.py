cf98111afec8fd1e7154c4a3cd433619c86959e0
shubham <patelsubham7777@gmai.com>
2024-11-24 21:03:14 +0530

    updating data source for login TC

--- page/loginpage.py
+++ page/loginpage.py
@@ -6,19 +6,30 @@ class LoginPage:
     def __init__(self,driver):
         self.driver=driver
 
-    email_id = "Email"
-    password_id = "Password"
-    login_button_Xpath = "//input[@value='Log in']"
-    invalid_login_error_message =  "//div[@class='validation-summary-errors']/span"
+    email_id = "Email"      #id
+    password_id = "Password"        #id
+    login_button_Xpath = "//input[@value='Log in']"     #xpath
+    invalid_login_error_message_common =  "//div[@class='validation-summary-errors']/span"     #xpath
+    invalid_email_error_message = "//li[contains(text(), 'No customer account found')]"
+    invalid_password_error_message = "//li[contains(text(),'The credentials provided are incorrect')]"
+    logout = "//a[text()='Log out']"
+    login = "//a[text()='Log in']"
 
     def enter_email_password_and_click_login(self, email,password):
         self.driver.find_element(By.ID, self.email_id).send_keys(email)
         self.driver.find_element(By.ID, self.password_id).send_keys(password)
         self.driver.find_element(By.XPATH, self.login_button_Xpath).click()
 
-    def verify_invalid_login_error_message(self):
-        error_message = self.driver.find_element(By.XPATH, self.invalid_login_error_message).text
-        assert error_message == 'Login was unsuccessful. Please correct the errors and try again.'
+    def verify_invalid_login_error_message(self, error_message):
+        error_message = self.driver.find_element(By.XPATH, self.invalid_login_error_message_common).text
+        assert error_message == error_message
+
+    def logout_and_verify_login_link_is_visible(self):
+        self.driver.find_element(By.XPATH, self.logout).is_displayed()
+        self.driver.find_element(By.XPATH, self.logout).click()
+        self.driver.find_element(By.XPATH, self.login).is_displayed()
+
+
 
 
 
--- utilities/data_source.py
+++ utilities/data_source.py
@@ -1,6 +1,29 @@
 from utilities import read_utils
 
+"""
+test invalid login data consist following:
+Blank email, Blank password
+Blank email, Invalid password
+Blank email, Valid password
+Invalid email, Blank password
+Invalid email, Invalid password
+Invalid email, Valid password
+Valid email, Blank password
+Valid email, Invalid password
+"""
+
 test_invalid_login_data = [
-     ("saul", "saul123", "Invalid credentials"),
-     ("kim", "kim123", "Invalid credentials")
+     ("", "", "Login was unsuccessful. Please correct the errors and try again."),
+     ("", "Invalid", "Login was unsuccessful. Please correct the errors and try again."),
+     ("", "Demo@123", "Login was unsuccessful. Please correct the errors and try again."),
+     ("invalid@gmail.com", "", "Login was unsuccessful. Please correct the errors and try again."),
+     ("invalid@gmail.com", "Invalid", "Login was unsuccessful. Please correct the errors and try again."),
+     ("invalid@gmail.com", "Demo@123", "Login was unsuccessful. Please correct the errors and try again."),
+     ("shuham122@gmail.com", "", "Login was unsuccessful. Please correct the errors and try again."),
+     ("shuham122@gmail.com", "Invalid", "Login was unsuccessful. Please correct the errors and try again."),
+    ]
+
+test_valid_login_data = [
+     ("shuham122@gmail.com", "Demo@123"),
+     ("demo@shubham.com", "demo@shubham.com")
     ]
