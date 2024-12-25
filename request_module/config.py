60e75c0e28ad9ee951c28326f5c24a9736f9f8ef
shubham <patelsubham7777@gmai.com>
2024-12-25 21:26:36 +0530

    post request completed

--- request_module/config.py
+++ request_module/config.py
@@ -5,7 +5,7 @@ VERSION = 'v2/'
 USERS = 'users'
 
 # We can add this in Env Variable but not added.
-Access_Token = "0d455c287c3981c0aacbd89a792dada80b2dd4ec60a06a574fa0772881abb7adefgh"
+Access_Token = "0d455c287c3981c0aacbd89a792dada80b2dd4ec60a06a574fa0772881abb7ad"
 
 def users():
     return BASE_URL + BASE_PATH + VERSION + USERS
--- request_module/post.py
+++ request_module/post.py
@@ -1,10 +1,26 @@
+import requests
+
 import config
 
 """ for add/post the users """
 
 url = config.users()
 
-name = "shubham"
-email = "demo@demo.com"
-Gender = "male"
-status = "active"
+# take the user input
+name = input("Enter the name: ")
+email = input("Enter the email: ")
+gender = input("Enter the gender: ")
+status = input("Enter the status: ")
+
+# create data for the input
+data = dict()
+data['name'] = name
+data['email'] = email
+data['gender'] = gender
+data['status'] = status
+
+headers = dict()
+headers['Authorization'] = 'Bearer ' + config.Access_Token
+
+post = requests.post(url, data=data, headers=headers)
+print(post.json())
